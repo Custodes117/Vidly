e4b03782676a28f117ec19b33ae8da9b427dddff
Custodes117 <masterdiver117@yahoo.de>
2019-12-30 10:51:10 +0100

    -add customer list and customer details

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public static CustomerViewModel CustomersVm { get; private set; }
+
+        static CustomerController()
+        {
+            CustomersVm = new CustomerViewModel()
+            {
+                Customers = new List<Customer>
+             {
+                 new Customer{ Name = "Customer 1", Id = 1},
+                new Customer{ Name = "Customer 2", Id = 2}
+             }
+            };
+        }
+                
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(CustomersVm);
+        }
+
+        // GET: Customer/CustomerDetails
+        public ActionResult CustomerDetails(int id)
+        {
+            //TODO: show "Not found"
+            return View(CustomersVm.Customers.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
